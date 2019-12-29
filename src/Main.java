78b14e760c4b39d8884d841240d7d167f9a803c6
Ledia Isaj <ledia_isai@hotmail.com>
2019-12-29 12:16:44 +0100

    Experiment 1.2: random reading
    closes #7

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import experiment.RandomReading;
 import experiment.SequentialReading;
 
 import java.io.IOException;
@@ -6,5 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         SequentialReading experiment1 = new SequentialReading();
         System.out.println(experiment1.length1("input/kind_type.csv"));
+        RandomReading experiment2 = new RandomReading();
+        System.out.println(experiment2.randjump1("input/kind_type.csv", 200));
     }
 }
